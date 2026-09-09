436f8a69c7ab3086e85f8955a88d5e7e11194d9b
YushaRencuzogullari <yrencuz1@gmail.com>
2026-09-09 11:09:25 -0700

    add users data structure

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,6 +6,10 @@ const port = 8000;
 
 app.use(express.json());
 
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -13,3 +17,33 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
